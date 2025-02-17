548b3513825df8ea48c2e121c76e7cb01cbd4260
Sadikshya Banstola <118067710+sadibanstola@users.noreply.github.com>
2025-02-17 23:59:35 +0545

    updated the services page

--- html/e_sewa_payment.php
+++ html/e_sewa_payment.php
@@ -1,13 +1,13 @@
 <?php
-// e_seva_payment.php
+// e_sewa_payment.php
 
 // Sample data, replace with actual API request data
 $amount = 1500; // Total amount to be paid
-$paymentGatewayUrl = "https://api.e-seva.com/payment"; // Hypothetical E-seva API URL
+$paymentGatewayUrl = "https://api.e-sewa.com/payment"; // Hypothetical E-sewa API URL
 
-// Redirect to the E-seva payment gateway with parameters
+// Redirect to the E-sewa payment gateway with parameters
 echo "<form action='{$paymentGatewayUrl}' method='POST'>
     <input type='hidden' name='amount' value='{$amount}'>
-    <input type='submit' value='Pay with E-seva'>
+    <input type='submit' value='Pay with E-sewa'>
 </form>";
 ?>
